f035d87f03080efda1c5b7e5e53f5eca9505b087
Mike Chau <mcxii@yahoo.com>
2013-05-16 09:40:15 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ScalpWebAnalytics::Application.config.secret_token = 'a851428ca2d700ed626a8b007742422cb1e79ec6b7a8d7772e16469b50d274d9eefc75f7016410311b07b3d7309fb01ab99157874d74b6d74a77b578b48e98c8'
