910efd9d5497a3598892b02ceb57db5ab88094e3
Mike Chau <mchau@iwu.edu>
2013-05-24 09:55:06 -0500

    Add omniauth check for User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,4 +8,20 @@ class User < ActiveRecord::Base
   # Setup accessible (or protected) attributes for your model
   attr_accessible :email, :password, :password_confirmation, :remember_me, :provider, :uid, :username, :avatar
   # attr_accessible :title, :body
+
+  def self.from_omniauth(auth)
+    if user = User.find_by_email(auth.info.email)
+      user.provider = auth.provider
+      user.uid = auth.uid
+      user
+    else
+      where(auth.slice(:provider, :uid)).first_or_create do |user|
+        user.provider = auth.provider
+        user.uid = auth.uid
+        user.username = auth.info.name
+        user.email = auth.info.email
+        user.avatar = auth.info.image
+      end
+    end
+  end
 end
